58a12e010c540c395a92e6fe42ffc65407f96cc9
NandoMiniom2 <122196385+NandoMiniom2@users.noreply.github.com>
2026-09-13 10:08:11 -0300

    Refactor DoppelgangerEntityRenderer imports and comments
    Updated import statement for PlayerSkinProvider and removed outdated comments.

--- doppelgangerz-fabric/src/main/java/com/doppelgangerz/client/render/DoppelgangerEntityRenderer.java
+++ doppelgangerz-fabric/src/main/java/com/doppelgangerz/client/render/DoppelgangerEntityRenderer.java
@@ -3,19 +3,12 @@ package com.doppelgangerz.client.render;
 import com.doppelgangerz.entity.DoppelgangerEntity;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.PlayerSkinProvider;
+import net.minecraft.client.texture.PlayerSkinProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.util.Identifier;
 
-/**
- * Renderiza o Doppelganger usando a skin do jogador copiado (Secao 6).
- *
- * Nesta Beta nao ha corrupcao visual progressiva - a textura usada e sempre
- * a skin normal do jogador. Corrupcao gradual (Secoes 6 e 38) fica para a
- * proxima iteracao, ver README_BETA.md.
- */
 public class DoppelgangerEntityRenderer extends MobEntityRenderer<DoppelgangerEntity, PlayerEntityModel<DoppelgangerEntity>> {
 
     private static final Identifier FALLBACK_SKIN = new Identifier("minecraft", "textures/entity/steve.png");
