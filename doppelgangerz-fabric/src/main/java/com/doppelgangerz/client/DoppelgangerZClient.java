02eda190ff117dfc353329bb597808ce49182703
NandoMiniom2 <122196385+NandoMiniom2@users.noreply.github.com>
2026-09-13 10:07:30 -0300

    Fix import statement for EntityRendererRegistry

--- doppelgangerz-fabric/src/main/java/com/doppelgangerz/client/DoppelgangerZClient.java
+++ doppelgangerz-fabric/src/main/java/com/doppelgangerz/client/DoppelgangerZClient.java
@@ -3,7 +3,7 @@ package com.doppelgangerz.client;
 import com.doppelgangerz.client.render.DoppelgangerEntityRenderer;
 import com.doppelgangerz.registry.ModEntities;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 
 public class DoppelgangerZClient implements ClientModInitializer {
     @Override
